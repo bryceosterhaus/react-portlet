2c845fff2772b0c33a00a16ebcfd7fab5f383a9d
Bryce Osterhaus <bryce.osterhaus@liferay.com>
2016-08-29 11:51:21 -0700

    Add webpack dev-server

--- src/main/resources/META-INF/resources/view.jsp
+++ src/main/resources/META-INF/resources/view.jsp
@@ -2,6 +2,8 @@
 
 <div id="helloReactApp"></div>
 
+<script src="/o/hello-react-1.0.0/js/dist/bundle.js"></script>
+
 <aui:script>
 	window.App(document.getElementById('helloReactApp'));
 </aui:script>
--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,13 @@ var webpack = require('webpack');
 
 module.exports = {
 	entry: "./src/main/resources/META-INF/resources/js/src/main.js",
+	devServer: {
+		port: 3000,
+		proxy: {
+			'**': 'http://localhost:8080'
+		},
+		publicPath: '/o/hello-react-1.0.0/js/dist/'
+	},
 	output: {
 		path: './src/main/resources/META-INF/resources/js/dist',
 		filename: "bundle.js"
