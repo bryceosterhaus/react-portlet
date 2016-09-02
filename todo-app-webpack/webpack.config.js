0b277ba10a0577949d35f5aa9bbfe401fd090c3b
Michael Hadley <michael.hadley@liferay.com>
2016-09-01 17:03:00 -0700

    Simplify webpack config

--- todo-app-webpack/webpack.config.js
+++ todo-app-webpack/webpack.config.js
@@ -1,32 +1,32 @@
+var path = require('path');
 var webpack = require('webpack');
 
+var JS_PUBLIC = '/o/todo-app-webpack-1.0.0/js/dist/';
+
+var JS_SRC = path.resolve(__dirname, 'src/main/resources/META-INF/resources/js/src/');
+
 module.exports = {
-	entry: [
-		'webpack-dev-server/client?http://0.0.0.0:3000', // WebpackDevServer host and port
-		'webpack/hot/only-dev-server', // "only" prevents reload on syntax errors
-		'./src/main/resources/META-INF/resources/js/src/main.js' // Your appʼs entry point
-	],
 	devServer: {
 		hot: true,
 		port: 3000,
 		proxy: {
 			'**': 'http://0.0.0.0:8080'
 		},
-		publicPath: 'http://0.0.0.0:3000/o/todo-app-webpack-1.0.0/js/dist/'
+		publicPath: JS_PUBLIC
 	},
-	output: {
-		path: './src/main/resources/META-INF/resources/js/dist',
-		filename: "bundle.js",
-		publicPath: '/o/todo-app-webpack-1.0.0/js/dist/'
-	},
+	entry: [
+		'webpack-dev-server/client?http://0.0.0.0:3000', // WebpackDevServer host and port
+		'webpack/hot/only-dev-server', // "only" prevents reload on syntax errors
+		path.resolve(JS_SRC, 'main.js') // Your appʼs entry point
+	],
 	module: {
 		loaders: [
 			{
-				exclude: /node_modules/,
+				include: JS_SRC,
 				loader: 'react-hot'
 			},
 			{
-				exclude: /node_modules/,
+				include: JS_SRC,
 				loader: ['babel'],
 				query: {
 					presets: ['es2015', 'react']
@@ -34,12 +34,12 @@ module.exports = {
 			}
 		]
 	},
+	output: {
+		filename: 'bundle.js',
+		path: './src/main/resources/META-INF/resources/js/dist',
+		publicPath: JS_PUBLIC
+	},
 	plugins: [
-		new webpack.DefinePlugin({
-			"process.env": {
-				NODE_ENV: JSON.stringify("production")
-			}
-		}),
 		new webpack.HotModuleReplacementPlugin()
 	]
 };
